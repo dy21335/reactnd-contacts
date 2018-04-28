54e7588fa2a0149f8a0555cc89fdacc30a6eb800
dy21335 <daisy_fyx@163.com>
2018-04-28 14:47:08 +0800

    基本功能实现

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './utils/ListContacts.js'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './utils/CreateContact'
+
+var contacts = [
+    // {
+    //   "id": 'ryan',
+    //   "name": 'Ryan Florence',
+    //   "email": 'ryan@reacttraining.com',
+    //   "avatarURL":"http://localhost:5001/ryan.jpg"
+    // },
+    // {
+    //   "id": 'michael',
+    //   "name": 'Michael Jackson',
+    //   "email": 'michael@reacttraining.com',
+    //   "avatarURL": "http://localhost:5001/michael.jpg"
+    // },
+    // {
+    //   "id": 'tyler',
+    //   "name": 'Tyler McGinnis',
+    //   "email": 'tyler@reacttraining.com',
+    //   "avatarURL": "http://localhost:5001/tyler.jpg"
+    // }
+  ];
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      screen : 'list',
+      contacts
+    }
+    this.removeContact = this.removeContact.bind(this);
+  }
+
+  removeContact(contact){
+    this.setState((pre) => ({
+      //es6箭头函数返回一个对象，要给对象加圆括号
+     contacts : this.state.contacts.filter((c) => c.id !== contact.id)
+    })
+  )
+
+    ContactsAPI.remove(contact)
+
+  }
+
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(
+        {contacts}
+      )
+    })
+  }
+
+
   render() {
+    //如果第一个表达式的值为true，运行第二表达式
     return (
-      <div>
-        Hello World
+      <div className='app'>
+
+        {this.state.screen === 'list' && (
+          <ListContacts
+            onDeleteContact = {this.removeContact}
+            contacts = {this.state.contacts}/>
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
+
+
       </div>
+
     )
   }
 }
---
+++ src/utils/ListContacts.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListContacts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+    this.updateQuery = this.updateQuery.bind(this);
+    this.clearQuery = this.clearQuery.bind(this);
+
+  }
+
+  updateQuery(query){
+    this.setState({query: query.trim()})
+  }
+
+  clearQuery(){
+    this.setState({ query : '' })
+  }
+
+
+    render()
+    {
+      const { contacts, onDeleteContact } = this.props;
+      const { query } = this.state;
+
+
+      let showingContacts;
+      if (query){
+          const match = new RegExp(escapeRegExp(query),'i');
+          //escapeRegExp表示忽略特殊字符，i表示忽略大小写
+          showingContacts = contacts.filter((contact) => match.test(contact.name))
+      }else{
+        showingContacts = contacts
+      }
+
+      showingContacts.sort(sortBy('name'));
+
+      return (
+        <div className='list-contacts'>
+          {JSON.stringify(this.state)}
+          <div className='list-contacts-top'>
+            <input
+              className='search-contacts'
+              type='text'
+              placeholder='Search contacts'
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+            <a
+              className='add-contact'
+              herf = "#create"
+              onClick={() => {}}
+            ></a>
+          </div>
+
+
+
+
+          {showingContacts.length !== contacts.length && (
+            <div className='showing-contacts'>
+              <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+              <button onClick={this.clearQuery}>Show all</button>
+
+            </div>
+          )}
+
+
+          <ol className='contact-list'>
+            {showingContacts.map((contact) =>
+              <li key={contact.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }
+                }/>
+                <div className='contact-details'>
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+                  Remove
+                </button>
+              </li>
+            )}
+          </ol>
+        </div>
+      )
+    }
+
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+}
+
+export default ListContacts
